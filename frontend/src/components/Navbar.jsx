ef9c6750634875c65c8ae932ff8ed98ce12ab417
Rahul Reekhadi <rahulreekhadi87@gmail.com>
2025-07-22 07:54:18 +0530

    Frotnend doctorAppointment

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import assets from '../assets/assets';
+import {NavLink, useNavigate} from 'react-router-dom';
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  const handleClick = ()=>{
+    navigate('/login');
+  }
+
+  const [showMenu,setShowMenu] = useState(false);
+  const [token,setToken] = useState(true);
+
+  return (
+    <div className='flex items-center justify-between p-md border-b border-lightGray'>
+        <img className='w-44 cursor-pointer' src={assets.logo} alt=""/>
+        <ul className='hidden md:flex gap-mxd items-start font-medium '>
+          <NavLink to='/'>
+            <li className='py-xs'>Home</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+          </NavLink>
+          <NavLink to='/doctors'>
+            <li className='py-xs'>All Doctors</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+          </NavLink>
+          <NavLink to='about'>
+            <li className='py-xs'>About</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+          </NavLink>
+          <NavLink to='contacts'>
+            <li className='py-xs'>Contacts</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+          </NavLink>
+        </ul>
+        <div className='flex items-center gap-xl'>
+          {token 
+          ? <div className='flex items-center gap-sm cursor-pointer group relative'>
+              <img className='rounded-full w-8' src={assets.profile_pic} alt=""/>
+              <img className='w-2.5 ' src={assets.dropdown_icon} alt=""/>
+              <div className='absolute top-0 pt-7xl text-smx font-medium text-midGray z-20 hidden group-hover:block '>
+                <div className='min-w-48 rounded flex flex-col gap-smx p-smx bg-stoneGray  '>
+                  <p onClick={()=>{navigate('/my-profile')}} className='hover:text-black'>My Profile</p>
+                  <p onClick={()=>{navigate('/my-appointment')}} className='hover:text-black'>My Appointments</p>
+                  <p onClick={()=>{setToken(false)}} className='hover:text-black'>Logout</p>
+                </div>
+              </div>
+          </div>:
+          <button onClick={handleClick} className='bg-primary text-white font-light px-xl py-smx rounded-full hidden md:block'>Create account</button>
+          }
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,78 @@
+/** @type {import('tailwindcss').Config} */
+import plugin from 'tailwindcss/plugin'
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontSize:{
+        sm:"var(--base)",
+        "smx":"var(--1x5base)",
+        xl:"var(--4xbase)"
+      },
+      padding:{
+        xs:"var(--05xbase)",
+        sm:"var(--base)",
+        "smx":"var(--1x5base)",
+        md:"var(--2xbase)",
+        xl:"var(--4xbase)",
+        "7xl":"var(--7xbase)"
+      },
+      colors:{
+          "stoneGray":"#f5f5f4",
+          "lightGray":"#9ca3af",
+          "midGray":"#4b5563",
+          "primary":"#5f6fff"
+      },
+      gap:{
+        xs:"var(--05xbase)",
+        "smx":"var(--1x5base)",
+        sm:"var(--base)",
+        md:"var(--2xbase)",
+        "mxd":"var(--2x5base)",
+        xl:"var(--4xbase)"
+      }
+    },
+  },
+  plugins: [
+    plugin(function({addBase}){
+      addBase({
+      ':root': {
+        '--base': 'calc(8 * 1px)',
+        '--0x25base': 'calc(var(--base) * 0.25)',
+        '--0x5base': 'calc(var(--base) * 0.5)',
+        '--0x75base': 'calc(var(--base) * 0.75)',
+        '--1xbase': 'calc(var(--base) * 1)',
+        '--1x25base': 'calc(var(--base) * 1.25)',
+        '--1x5base': 'calc(var(--base) * 1.5)',
+        '--1x75base': 'calc(var(--base) * 1.75)',
+        '--2xbase': 'calc(var(--base) * 2)',
+        '--2x25base': 'calc(var(--base) * 2.25)',
+        '--2x5base': 'calc(var(--base) * 2.5)',
+        '--2x75base': 'calc(var(--base) * 2.75)',
+        '--3xbase': 'calc(var(--base) * 3)',
+        '--3x25base': 'calc(var(--base) * 3.25)',
+        '--3x5base': 'calc(var(--base) * 3.5)',
+        '--3x75base': 'calc(var(--base) * 3.75)',
+        '--4xbase': 'calc(var(--base) * 4)',
+        '--4x5base': 'calc(var(--base) * 4.5)',
+        '--5xbase': 'calc(var(--base) * 5)',
+        '--5x5base': 'calc(var(--base) * 5.5)',
+        '--6xbase': 'calc(var(--base) * 6)',
+        '--6x5base': 'calc(var(--base) * 6.5)',
+        '--7xbase': 'calc(var(--base) * 7)',
+        '--7x5base': 'calc(var(--base) * 7.5)',
+        '--8xbase': 'calc(var(--base) * 8)',
+        '--8x5base': 'calc(var(--base) * 8.5)',
+        '--9xbase': 'calc(var(--base) * 9)',
+        '--9x5base': 'calc(var(--base) * 9.5)',
+        '--10xbase': 'calc(var(--base) * 10)',
+        },
+      });
+      
+    })
+  ],
+}
+
