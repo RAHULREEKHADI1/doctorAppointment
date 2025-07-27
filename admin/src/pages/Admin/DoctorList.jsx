2d3c24ed7888b9371c6806c10d8461a6f1705617
Rahul Reekhadi <rahulreekhadi87@gmail.com>
2025-07-27 19:46:57 +0530

    Background remover from cloudinary

--- admin/src/pages/Admin/DoctorList.jsx
+++ admin/src/pages/Admin/DoctorList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import AdminContext from '../../context/AdminContext'
 
 const DoctorList = () => {
@@ -11,16 +11,31 @@ const DoctorList = () => {
     }
   },[token])
 
+  const removeBackground = (imageUrl) => {
+  return imageUrl.replace('/upload/', '/upload/e_background_removal/');
+  };
+
+
+  const [isChecked,setIsChecked] = useState(false);
+
   return (
-    <div>
-      <h1>All Doctors</h1>
-      <div>
+    <div className='m-mdx max-h-[90vh] overflow-y-scroll'>
+      <h1 className='text-lg font-medium'>All Doctors</h1>
+      <div className='w-full flex flex-wrap gap-md pt-mxd gap-y-lg'>
         {
           doctors.map((item,idx)=>
-            <div key={idx} >
+            <div className='border border-indigo-200 rounded-xl max-w-56 overflow-hidden cursor-pointer  group' key={idx} >
               {/* {console.log(item.image)
               } */}
-              <img src={item.image} alt='' />
+              <img className='bg-indigo-50 group-hover:bg-primary transition-all duration-400' src={removeBackground(item.image)} alt='' />
+              <div>
+                <p>{item.name}</p>
+                <p>{item.speciality}</p>
+                <div>
+                  <input type='checkbox' onChange={(e)=>setIsChecked(e.target.checked)} checked={item.available}/>
+                  <p>Available</p>
+                </div>
+              </div>
             </div>
           )
         }
